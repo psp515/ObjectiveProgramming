d57241c043cd86b098c16aa494f74578e98724d5
psp515 <kolberu@gmail.com>
2022-11-19 13:21:50 +0100

    Desc

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -14,28 +14,13 @@ public class World
 {
     public static void main(String[] args) {
 
+
         MoveDirection[] directions = new OptionsParser().parse(args);
         IWorldMap map;
         IEngine engine;
         ISwingEngine swingEngine;
         Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
 
-        map = new RectangularMap(10, 5);
-        engine = new SimulationEngine(directions, map, positions);
-        //out.println(map);
-        //engine.run();
-        //out.println(map);
-
-        //Swing
-        /*MoveDirection[] new_directions = new OptionsParser().parse(args);
-        map = new RectangularMap(10, 5);
-        Vector2d[] new_positions = { new Vector2d(2,2), new Vector2d(3,4) };
-        ISwingEngine new_engine = new SimulationEngine(new_directions, map, new_positions);
-        try {
-            new_engine.run(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }//*/
 
         map = new GrassField(10);
         engine = new SimulationEngine(directions, map, positions);
@@ -44,7 +29,6 @@ public class World
         out.println(map);
 
         //Swing Grass Field
-
         map = new GrassField(10);
         swingEngine = new SimulationEngine(directions, map, positions);
         try {
@@ -53,6 +37,7 @@ public class World
             throw new RuntimeException(e);
         }
 
+        /* Punkt 12 spełniony. */
         out.println("Finish");
     }
 }
